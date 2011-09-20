40553300d7ea63dc9f58903dfde8038ada8519df
Joel Heymbeeck <heymbeeck.joel@gmail.com>
2011-09-21 00:57:18 +0200

    Optimized
    Signed-off-by: Joel Heymbeeck <heymbeeck.joel@gmail.com>

--- Plugins/power.lua
+++ Plugins/power.lua
@@ -6,48 +6,70 @@ local classOptions = Options[class]
 if ( classOptions == nil ) then return end
 local O = classOptions["power"]
 if O.display ~= true then return end
+
 local kind = O.kind
+local color = RAID_CLASS_COLORS[T.myclass]
+local r, g, b
+r = color.r
+g = color.g
+b = color.b
+if ( O.color ) then
+	r, g, b = unpack(O.color)
+end
 
 local sPowerBG = CreateFrame("Frame", "sPowerBG", UIParent)
+if ( O.anchor ~= nil ) then
+	sPowerBG:CreatePanel(nil, O.width , O.height, unpack(O.anchor))
+else
+	sPowerBG:CreatePanel(nil, O.width , O.height, "TOPLEFT", sCombo[1], "BOTTOMLEFT", 0, -3)
+end
+
 local sPowerText = T.SetFontString(sPowerBG, C.media.font, 20, "THINOUTLINE")
 sPowerText:Point("TOP", sPowerBG, "BOTTOM", 0, -3)
-sPowerBG:CreatePanel(nil, O.width , O.height, "TOPLEFT", sCombo[1], "BOTTOMLEFT", 0, -3)
+
 sPowerBG.sStatus = CreateFrame("StatusBar", "sStatus", sPowerBG)
 sPowerBG.sStatus:SetStatusBarTexture(C.media.normTex)
+sPowerBG.sStatus:SetMinMaxValues(0, UnitPowerMax("player"))
+sPowerBG.sStatus:SetStatusBarColor(r, g, b)
 sPowerBG.sStatus:SetFrameLevel(6)
 sPowerBG.sStatus:Point("TOPLEFT", sPowerBG, "TOPLEFT", 2, -2)
 sPowerBG.sStatus:Point("BOTTOMRIGHT", sPowerBG, "BOTTOMRIGHT", -2, 2)
-sPowerBG.sStatus:SetScript("OnUpdate", function()
+
+if ( O.text == true ) then
+	sPowerBG.text = sPowerBG.sStatus:CreateFontString(nil, "OVERLAY")
+	sPowerBG.text:SetFont(C.media.uffont, 12)
+	sPowerBG.text:Point("CENTER", sPowerBG.sStatus)
+	sPowerBG.text:SetShadowColor(0, 0, 0)
+	sPowerBG.text:SetShadowOffset(1.25, -1.25)
+end
+
+local OnUpdate = function(self)
 	local value = UnitPower("player")
-    sPowerBG.sStatus:SetMinMaxValues(0, UnitPowerMax("player"))
-    sPowerBG.sStatus:SetValue(value)
+    self.sStatus:SetValue(value)
 
-	local color = RAID_CLASS_COLORS[T.myclass]
-	local r, g, b
-	r = color.r
-	g = color.g
-	b = color.b
-	if ( O.color ) then
-		r, g, b = unpack(O.color)
-	end
-	sPowerBG.sStatus:SetStatusBarColor(r, g, b)
-	--sPowerBG.sStatus:SetStatusBarColor( unpack( color ) )
-
-	--DEFAULT_CHAT_FRAME:AddMessage("Value: "..value)
 	if ( O.text == true ) then
-		sPowerText:SetText(value)
+		sPowerBG.text:SetText(value)
 	end
-end)
+end
+
+local OnShow = function(self)
+	self:SetScript("OnUpdate", OnUpdate)
+end
+
+local OnHide = function(self)
+	self:SetScript("OnUpdate", nil)
+end
+
 sPowerBG:RegisterEvent("PLAYER_ENTERING_WORLD")
 sPowerBG:RegisterEvent("UNIT_DISPLAYPOWER")
 sPowerBG:RegisterEvent("PLAYER_REGEN_DISABLED")
 sPowerBG:RegisterEvent("PLAYER_REGEN_ENABLED")
 sPowerBG:RegisterEvent("UNIT_POWER")
-if ( O.autohide == true ) then
-	sPowerBG:SetScript("OnEvent", function(self, event)
-		local p, pname = UnitPowerType("player")
-		--DEFAULT_CHAT_FRAME:AddMessage("Power: "..p.."  "..pname)
-		--if p == SPELL_POWER_ENERGY then
+sPowerBG:SetScript("OnEvent", function(self, event)
+	local valueMax = UnitPowerMax("player")
+	self.sStatus:SetMinMaxValues(0, valueMax)
+	local p, pname = UnitPowerType("player")
+	if ( O.autohide == true ) then
 		if p == kind then
 			if event == "PLAYER_REGEN_DISABLED" then
 				sPowerBG:Show()
@@ -61,5 +83,11 @@ if ( O.autohide == true ) then
 		else
 			sPowerBG:Hide()
 		end
-	end)
-end
+	else
+		sPowerBG:Show()
+		self:SetScript("OnUpdate", OnUpdate) -- TODO: why OnShow is not called
+	end
+end)
+
+sPowerBG:SetScript("OnShow", OnShow) -- This is what stops constant OnUpdate
+sPowerBG:SetScript("OnHide", OnHide)
